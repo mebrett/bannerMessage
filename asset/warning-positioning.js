9e5252bfa811573b2ad587eb5c94596470fba7e2
Megan R Brett <mbrett@gmu.edu>
2022-01-31 14:52:05 -0500

    Update warning-positioning.js
    remove html from bannerText

--- asset/warning-positioning.js
+++ asset/warning-positioning.js
@@ -1,6 +1,6 @@
 (function($) {
   $(document).ready(function() {
-    var bannerText = 'This site contains historical content which may be upsetting or offensive to some visitors. Please see the <a href="https://hearingtheamericas.org/s/the-americas/page/content">content warning</a> for a full statement.';
+    var bannerText = 'This site contains historical content which may be upsetting or offensive to some visitors. Please see the content warning for a full statement.';
 
     var buttonHtml = '<a href="' + donationUrl + '" class="donate-button"><img title="Donate to RRCHNM" src="https://rrchnm.org/donate/donate-button-red.svg"></a>';
     var bannerHtml = '<div id="support-rrchnm-banner">' + bannerText + buttonHtml + '</div>';
