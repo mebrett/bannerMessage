2954d4964c35773ff54f34cb0ffe0becb49a6b6b
Megan R Brett <mbrett@gmu.edu>
2022-01-31 14:54:13 -0500

    Update warning-positioning.js

--- asset/warning-positioning.js
+++ asset/warning-positioning.js
@@ -3,7 +3,7 @@
     var bannerText = 'This site contains historical content which may be upsetting or offensive to some visitors. Please see the content warning for a full statement.';
 
     var buttonHtml = '<a href="' + donationUrl + '" class="donate-button"><img title="Donate to RRCHNM" src="https://rrchnm.org/donate/donate-button-red.svg"></a>';
-    var bannerHtml = '<div id="support-rrchnm-banner">' + bannerText + buttonHtml + '</div>';
+    var bannerHtml = '<div id="conent-warning-banner">' + bannerText + buttonHtml + '</div>';
     var banner = $(bannerHtml);
     
     if ($('body').hasClass('admin-bar')) {
